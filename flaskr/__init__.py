e0600186917be5b506300ae7be07ac7a06585831
ddns0098 <d.dns0098g@gmail.com>
2018-12-28 17:48:38 +0100

    refactor(db)

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,6 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_oauthlib.client import OAuth
 
 # create and configure the app
 app = Flask(__name__, instance_relative_config=True)
@@ -10,20 +9,13 @@ app.config.from_pyfile('config.py')
 app.config.from_mapping(
     SECRET_KEY='dev',
     SQLALCHEMY_DATABASE_URI='sqlite:///site.db',
-    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
 )
 db = SQLAlchemy(app)
 
 from flaskr import routes
-
-# load the instance config, if it exists, when not testing
-app.config.from_pyfile('config.py', silent=True)
 
 # ensure the instance folder exists
 try:
     os.makedirs(app.instance_path)
 except OSError:
     pass
-
-from . import db
-db.init_app(app)
